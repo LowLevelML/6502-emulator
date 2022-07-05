22e0fb8d148659f21c6ce25369c8a752a1caf449
Thamognya Kodi <contact@thamognya.com>
2022-07-05 18:53:54 +0700

    episode 1: struct cpu done. video 1 not done

---
+++ README.md
@@ -0,0 +1,24 @@
+# 6502 Emulator
+
+## Resources
+- [first video](https://youtu.be/qJgsuQoy9bc) (just go to the channel or the playlist for the other videos)
+- [https://www.obelisk.me.uk/6502/](https://web.archive.org/web/20210912192127/https://www.obelisk.me.uk/6502/) the website it self is shut down.
+- [https://sta.c64.org/cbm64mem.html](https://sta.c64.org/cbm64mem.html) and in case it dies of [way back machine version of it]()
+
+## How to use my code and resources
+
+open the video and follow the website contents while taking a look at my notes (after each episode my commits have done with episode _n_ so you can take a look at them at that point) 
+
+## Notes 
+
+These are present in the code
+
+## Code Structure
+
+## Video 1
+
+[https://github.com/davepoo/6502Emulator/tree/2b48db0d0fb41f416f4c1e49f0a2d6df184efa0b](https://github.com/davepoo/6502Emulator/tree/2b48db0d0fb41f416f4c1e49f0a2d6df184efa0b)
+
+proccesor status flags
+
+![proccesor_status_register_flags](./assets/proccesor_status_register_flags.png)
---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// for c++ data types values take a look at https://www.tutorialspoint.com/cprogramming/c_data_types.htm and 
+
+/*Define byte and 2 bytes*/
+//using Byte = unsigned char; // that is a byte or 8 bits
+//using Word = unsigned short; // 2 bytes or 16 bits
+/*for c*/
+typedef unsigned char Byte;
+typedef unsigned short Word;
+// For the registers and why I am using a certain amount of bits or bytes take a look at: https://web.archive.org/web/20210501031403/http://www.obelisk.me.uk/6502/registers.html
+
+struct CPU
+{
+    /*Define Program Counter*/
+    Word PC; // program counter which is 16 bit register
+
+    /*Define Stack Pointer*/
+    // stack is a small piece of resuable memory for executing you push and pop things of the stack. It is just a small piece of memory that gets reused.
+    // https://en.wikipedia.org/wiki/Stack_register
+    Word SP;
+
+    /*Define The 3 8bit registers*/
+    // since the registers are 8 bit the cpu (6502) is 8 bit
+    Byte A, X, Y;
+
+    /*Define the processor status*/
+    // the processor status is a 1 byte with a bunch of bits with flags in it that gets set deppending on what instrutions happen (same thing on modern processor with differnt flags and etc.)
+    // look at readme image for video 1 for the flags
+    // c++ and c supports bitfield
+    Byte C : 1; // carry
+    Byte Z : 1; // zero
+    Byte I : 1; // irq disable
+    Byte D : 1; // decimal mode
+    Byte B : 1; // break command
+    Byte V : 1; // overflow
+    Byte N : 1; // negative
+};
+
+int main()
+{
+    return 0;
+}
